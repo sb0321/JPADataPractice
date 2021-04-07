fe0d0e76ff2b423168ad858b8188d1201b307b16
sb0321 <woltaryksb1@gmail.com>
2021-04-07 17:07:14 +0900

    init commit

---
+++ src/main/java/me/whiteship/demospringdata/JpaRunner.java
@@ -0,0 +1,58 @@
+package me.whiteship.demospringdata;
+
+import org.hibernate.Session;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Component
+@Transactional
+public class JpaRunner implements ApplicationRunner {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+//        Post post = new Post();
+//        post.setTitle("Spring Data JPA 언제 보나...");
+//
+//        Comment comment = new Comment();
+//        comment.setComment("빨리 보고 싶다.");
+//        post.addComment(comment);
+//
+//        Comment comment1 = new Comment();
+//        comment1.setComment("곧 보여드릴게요");
+//        post.addComment(comment1);
+
+        Session session = entityManager.unwrap(Session.class);
+
+        Post post = session.get(Post.class, 5l);
+        session.delete(post);
+
+//        Account account = new Account();
+//        account.setUsername("keesun2");
+//        account.setPassword("jpa");
+//
+//        Study study = new Study();
+//        study.setName("Spring Data JPA");
+//
+//        account.addStudy(study);
+//
+//        Session session = entityManager.unwrap(Session.class);
+//        session.save(account);
+//        session.save(study);
+//
+//        Account keesun = session.load(Account.class, account.getId());
+//        keesun.setUsername("whiteship");
+//        keesun.setUsername("keesun");
+//        keesun.setUsername("keesun2");
+//        System.out.println("========================");
+//        System.out.println(keesun.getUsername());
+    }
+}
---
+++ src/main/java/me/whiteship/demospringdata/Post.java
@@ -0,0 +1,45 @@
+package me.whiteship.demospringdata;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Post {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    @OneToMany(mappedBy = "post", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private Set<Comment> comments = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(Set<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public void addComment(Comment comment) {
+        comments.add(comment);
+    }
+}
